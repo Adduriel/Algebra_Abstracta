19e6a82c3a1144b67cb7e13d8fa03d10a8425b99
Adduriel <marela.mendoza@ucsp.edu.pe>
2022-07-02 19:37:50 -0500

    upgrade

---
+++ Permanente_2a/Componentes/generan_prim.py
@@ -0,0 +1,20 @@
+from Componentes.miller_rabin import miller_rabin
+from Componentes.comp_rand import comp_rand
+
+def BITS_R(b):
+    max = pow(2, b) - 1
+    n = comp_rand(0, max)
+    m = pow(2, b - 1) + 1
+    n = n | m
+
+    return n
+
+configure_s = 40
+
+def generan_prim(b):
+    s = configure_s
+    n = BITS_R(b)
+    while (not miller_rabin(n, s)):
+        n += 2
+  
+    return n
